3063bfecac2b79469326c38e2d81537d8a7db898
Martin Karugaba <martinkarugaba21@gmail.com>
2023-02-15 12:59:35 +0300

    Add items to homepage

--- src/modules/fetchData.js
+++ src/modules/fetchData.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://pokeapi.co/api/v2/pokemon?limit=12&offset=0';
-const cardsContainer = document.querySelector('.cards-container')
+const cardsContainer = document.querySelector('.cards-container');
 
 const fetchImage = async (url) => {
   const response = await fetch(url);
@@ -17,9 +17,13 @@ const fetchImage = async (url) => {
   cardsContainer.innerHTML += `
     <div class='card'>
       <img class="card__image" src=${front_default} alt=${name}/>
-      <p class="name">${name}</p>
+      <div class="title">
+        <p class="name">${name}</p>
+        <p class="card__likes">Likes <span>(1)</span> <i class="fa-solid fa-heart"></i></p>
+      </div>
+      <button class="comment__button">Comment</button>
     </div>
-  `
+  `;
 };
 
 export const fetchData = async () => {
