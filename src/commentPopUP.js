6c29dea5ca062dc34de32ac1955eaabad313e599
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-15 10:34:04 +0530

    pop up skelton

---
+++ src/commentPopUP.js
@@ -0,0 +1,46 @@
+const commentData = [
+  {
+    pokeImage: "A",
+    alt: "A",
+    pokemonName: "A",
+    pokemonType: "1 2 3 4",
+  },
+  {
+    pokeImage: "B",
+    alt: "B",
+    pokemonName: "B",
+    pokemonType: "1 2 3 4",
+  },
+  {
+    pokeImage: "C",
+    alt: "C",
+    pokemonName: "C",
+    pokemonType: "1 2 3 4",
+  },
+  {
+    pokeImage: "D",
+    alt: "D",
+    pokemonName: "D",
+    pokemonType: "1 2 3 4",
+  },
+  {
+    pokeImage: "E",
+    alt: "E",
+    pokemonName: "E",
+    pokemonType: "1 2 3 4",
+  },
+  {
+    pokeImage: "F",
+    alt: "F",
+    pokemonName: "F",
+    pokemonType: "1 2 3 4",
+  },
+]
+
+const popUpMAker = () => {
+  const commentBtn = document.querySelectorAll('.commentBtn')
+    //  this is left beacause i don't know the data type yet
+    // so i don't want to waste energy on it 
+}
+
+module.exports = popUpMAker;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import joke from './joke';
 import './styles/main.css';
+const popUpMAker = require('./commentPopUP.js')
 
+popUpMAker()
 console.log(joke());
 // console.log("mf")
