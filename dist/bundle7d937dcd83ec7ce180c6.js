c97f35753e1b15d1f21a8bad51b73299858de474
Martin Karugaba <martinkarugaba21@gmail.com>
2023-02-17 17:56:54 +0300

    new build

--- dist/bundle7d937dcd83ec7ce180c6.js
+++ dist/bundle7d937dcd83ec7ce180c6.js
@@ -928,6 +928,17 @@ fetchData();
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (dataBase);
 
 
+/***/ }),
+
+/***/ "./src/media/pokemon-logo.png":
+/*!************************************!*\
+  !*** ./src/media/pokemon-logo.png ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "a4a49d7a315b725de60c.png";
+
 /***/ })
 
 /******/ 	});
@@ -981,6 +992,18 @@ fetchData();
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -995,6 +1018,26 @@ fetchData();
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
@@ -1015,6 +1058,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/main.css */ "./src/styles/main.css");
 /* harmony import */ var _styles_home_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./styles/home.css */ "./src/styles/home.css");
 /* harmony import */ var _modules_comment_post_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/comment-post.js */ "./src/modules/comment-post.js");
+/* harmony import */ var _media_pokemon_logo_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./media/pokemon-logo.png */ "./src/media/pokemon-logo.png");
 
 
 
@@ -1024,4 +1068,4 @@ __webpack_require__.r(__webpack_exports__);
 
 /******/ })()
 ;
-//# sourceMappingURL=bundlecba0127b9f709c06fa88.js.map
+//# sourceMappingURL=bundle7d937dcd83ec7ce180c6.js.map
