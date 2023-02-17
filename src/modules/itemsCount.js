59f08174abd1e3449a8e704e0d9eba782d79ac9d
Martin Karugaba <martinkarugaba21@gmail.com>
2023-02-17 19:27:08 +0300

    edit itemsCount.js

--- src/modules/itemsCount.js
+++ src/modules/itemsCount.js
@@ -1,7 +1,6 @@
 const itemCounter = () => {
   const itemsCount = document.querySelector('.items-count');
   const items = document.querySelectorAll('.card');
-
   itemsCount.innerHTML = `(${items.length})`;
 };
 
